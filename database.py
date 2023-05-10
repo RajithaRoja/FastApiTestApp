76664e3d4d457511c4f31be67486dcb4749dfa57
RajithaRoja <115135606+RajithaRoja@users.noreply.github.com>
2023-05-10 17:55:07 +0530

    Update database.py
    database url updated

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:root@localhost/TodoApplicationDatabase'
+SQLALCHEMY_DATABASE_URL = 'postgresql://student_register_user:KchdMhwrdU4lZlkTEDe1gg1jenMkivH5@dpg-chdoklrhp8u3v72ff7v0-a/student_register'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
